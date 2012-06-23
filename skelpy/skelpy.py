004ef8fae0feaf328ddc85128103931fa7790735
Stefan Huchler <stefan@canta-game.org>
2012-06-24 00:25:19 +0200

    adds test/code for basic create from template function

--- skelpy/skelpy.py
+++ skelpy/skelpy.py
@@ -2,11 +2,31 @@
 #-*- coding:utf-8 -*-
 
 import os
+import shutil
 
 class SkelPy:
 	def __init__(self):
 		pass
 
-	def init(self):
-		if not os.path.exists('.skelpy'):
-			os.makedirs('.skelpy')
+	def init(self, path='.'):
+		skel_path = os.path.join('.', path, '.skelpy')
+		if os.path.exists(path):
+			if not os.path.isdir(path):
+				return 0
+		if not os.path.exists(skel_path):
+			os.makedirs(skel_path)
+
+	def create(self, source, target):
+		os.mkdir(target)
+		for root, dirs, files in os.walk(source):
+			target_root = root.replace(source,target,1)
+			for d in dirs:
+				if d != '.skelpy':
+					d_source = os.path.join(root, d)
+					d_target = os.path.join(target_root, d)
+					os.mkdir(d_target)
+					shutil.copystat(d_source, d_target)
+			for f in files:
+				f_source = os.path.join(root, f)
+				f_target = os.path.join(target_root, f)
+				shutil.copy(f_source, f_target)
--- tests/skelpy_tests.py
+++ tests/skelpy_tests.py
@@ -9,28 +9,58 @@ from time import sleep
 class Test_SkelPy(object):
 
 	def setup(self):
-                self.old_pwd = env['PWD']
-                self.temp_dir = tempfile.mkdtemp()
-                os.chdir(self.temp_dir)
+		self.old_pwd = env['PWD']
+		self.temp_dir = tempfile.mkdtemp()
+		os.chdir(self.temp_dir)
 		self.skel = SkelPy()
 
 	def teardown(self):
-                os.chdir(self.old_pwd)
-                shutil.rmtree(self.temp_dir)
+		os.chdir(self.old_pwd)
+		shutil.rmtree(self.temp_dir)
+
+	def test_template_path_is_file(self):
+		""" checks that a skelpy init dont override a file """
+		with open('template1', 'w') as f:
+				f.write('')
+		assert os.path.isfile('template1')
+		self.skel.init('template1')
+		assert os.path.isfile('template1')
 
 	def test_init_once(self):
-		""" tests if the tool creates the template dir """
-		self.skel.init()
-		template_path = os.path.join(self.temp_dir, '.skelpy')
-		assert os.path.isdir(template_path)
+		""" tests that the tool creates the template dir """
+		self.skel.init('template1')
+		skel_path = os.path.join(self.temp_dir, 'template1', '.skelpy')
+		assert os.path.isdir(skel_path)
 
 	def test_init_twice(self):
 		""" tests that 2nd init call dont overrides the template dir """
-		template_path = os.path.join(self.temp_dir, '.skelpy')
-                self.skel.init()
+		template_path = os.path.join(self.temp_dir, 'template1', '.skelpy')
+		self.skel.init('template1')
 		mod_time1 = os.path.getmtime(template_path)
 		sleep(0.001)
 		self.skel.init()
 		mod_time2 = os.path.getmtime(template_path)
 		assert mod_time1 == mod_time2
 
+	def test_create_files_once(self):
+		""" checks if create copies directories and files """
+		self.skel.init('template1')
+		os.chdir('template1')
+		for path in 'a','b','c':
+			os.mkdir(path)
+			with open('file_%s' % path, 'w+') as f:
+				f.write('0123456789abcdef')
+			os.chdir(path)
+		os.chdir(self.temp_dir)
+		self.skel.init('template1')
+		self.skel.create('template1', 'proj1')
+		assert os.path.isdir('proj1')
+		os.chdir('proj1')
+		for path in 'a','b','c':
+			files = os.listdir('.')
+			assert set(files) == set([path, "file_"+path]), files
+			assert os.path.isdir(path)
+			assert os.path.isfile("file_%s" % path)
+			f = open('file_%s' % path, 'r')
+			assert '0123456789abcdef' == f.read(), f
+			os.chdir(path)
