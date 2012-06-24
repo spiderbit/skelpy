328864b8ee4ee6d52448f839f4dd664561b7ea2b
Stefan Huchler <stefan@canta-game.org>
2012-06-25 00:14:18 +0200

    adds features/tests for ignore file support + only copy from templates

--- skelpy/skelpy.py
+++ skelpy/skelpy.py
@@ -9,6 +9,7 @@ class SkelPy:
 		pass
 
 	def init(self, path='.'):
+		""" initialise and mark a diretory as a template """
 		skel_path = os.path.join('.', path, '.skelpy')
 		if os.path.exists(path):
 			if not os.path.isdir(path):
@@ -16,17 +17,43 @@ class SkelPy:
 		if not os.path.exists(skel_path):
 			os.makedirs(skel_path)
 
+	def read_ignore_file(self, config_dir):
+		ignore_path = os.path.join(config_dir, 'ignore')
+		ignore_list = []
+		if os.path.isfile(ignore_path):
+			with open(ignore_path) as f:
+				for line in f:
+					ignore_list.append(line[:-1])
+		return ignore_list
+
+
 	def create(self, source, target):
-		os.mkdir(target)
-		for root, dirs, files in os.walk(source):
-			target_root = root.replace(source,target,1)
-			for d in dirs:
-				if d != '.skelpy':
-					d_source = os.path.join(root, d)
-					d_target = os.path.join(target_root, d)
-					os.mkdir(d_target)
-					shutil.copystat(d_source, d_target)
-			for f in files:
-				f_source = os.path.join(root, f)
-				f_target = os.path.join(target_root, f)
-				shutil.copy(f_source, f_target)
+		""" creates a new directory from the template """
+		config_dir = os.path.join(source, '.skelpy')
+		if os.path.isdir(config_dir):
+			ignore_list = self.read_ignore_file(config_dir)
+			os.mkdir(target)
+			for root, dirs, files in os.walk(source):
+				if root == source:
+					dirs.remove('.skelpy')
+				target_root = root.replace(source,target,1)
+				for d in dirs:
+					for i in ignore_list:
+						root2 = root.replace(source, '', 1)[1:]
+						if os.path.join(root2, d).startswith(i):
+							break
+					else:
+						d_source = os.path.join(root, d)
+						d_target = os.path.join(target_root, d)
+						os.mkdir(d_target)
+						shutil.copystat(d_source, d_target)
+				for f in files:
+					for i in ignore_list:
+						root2 = root.replace(source, '', 1)[1:]
+						if os.path.join(root2, f).startswith(i):
+							break
+					else:
+						f_source = os.path.join(root, f)
+						f_target = os.path.join(target_root, f)
+						assert f != 'c_file', f
+						shutil.copy(f_source, f_target)
--- tests/skelpy_tests.py
+++ tests/skelpy_tests.py
@@ -42,18 +42,22 @@ class Test_SkelPy(object):
 		mod_time2 = os.path.getmtime(template_path)
 		assert mod_time1 == mod_time2
 
-	def test_create_files_once(self):
-		""" checks if create copies directories and files """
-		self.skel.init('template1')
-		os.chdir('template1')
+
+	def create_sample_template(self, name):
+		os.chdir(name)
 		for path in 'a','b','c':
 			os.mkdir(path)
 			with open('file_%s' % path, 'w+') as f:
 				f.write('0123456789abcdef')
 			os.chdir(path)
 		os.chdir(self.temp_dir)
-		self.skel.init('template1')
-		self.skel.create('template1', 'proj1')
+
+	def test_create_copies_files(self):
+		""" checks if create copies directories and files """
+		name='template1'
+		self.skel.init(name)
+		self.create_sample_template(name)
+		self.skel.create(name, 'proj1')
 		assert os.path.isdir('proj1')
 		os.chdir('proj1')
 		for path in 'a','b','c':
@@ -64,3 +68,42 @@ class Test_SkelPy(object):
 			f = open('file_%s' % path, 'r')
 			assert '0123456789abcdef' == f.read(), f
 			os.chdir(path)
+
+	def test_create_copies_only_from_templates(self):
+		""" checks if create only accepts templates as source """
+		name='template1'
+		os.mkdir(name)
+		self.create_sample_template(name)
+		self.skel.create(name, 'proj1')
+		assert not os.path.isdir('proj1')
+
+	def test_create_dont_copy_ignore_files(self):
+		"""
+		checks if create dont copy files and dirs listed in the .skelpy/ignore file
+		"""
+		name='template1'
+		self.skel.init(name)
+		self.create_sample_template(name)
+		ignore_file = os.path.join(name, '.skelpy', 'ignore')
+		with open(ignore_file, 'w+') as f:
+			file_b = os.path.join('a','file_b') + "\n"
+			path_c = os.path.join('a','b','c') + "\n"
+			f.write(file_b)
+			f.write(path_c)
+		self.skel.create(name, 'proj1')
+		os.chdir('proj1')
+		for i in [['a', 'file_a'],['b', None],[None, 'file_c']]:
+			files = os.listdir('.')
+			d = i[0]
+			f = i[1]
+			if d == None:
+				i.remove(None)
+			if f == None:
+				i.remove(None)
+			assert set(files) == set(i), files
+			if d != None:
+				assert os.path.isdir(d)
+			if f != None:
+				assert os.path.isfile(f)
+			if d != None:
+				os.chdir(d)
